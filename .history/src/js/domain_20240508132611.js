47bcc9db40517ff181aa84b8c572c87444a641e2
“TatianaLevoshko” <delovoiy86@gmail.com>
2024-05-08 14:02:43 +0300

    Webpack

--- .history/src/js/domain_20240508132611.js
+++
@@ -1,9 +0,0 @@
-class Character {
-}
-
-export default class Character {
-	constructor(name, health) {
-	  this.name = name;
-	  this.health = health;
-	}
-  }
--- src/js/domain.js
+++
@@ -1,6 +0,0 @@
-export default class Character {
-	constructor(name, health) {
-	  this.name = name;
-	  this.health = health;
-	}
-  }
